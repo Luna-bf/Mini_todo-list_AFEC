a49f7efe65f62f7a6bef523cae1f657a277c5168
Luna-bf <lburattifayolle@gmail.com>
2025-09-26 15:17:58 +0200

    Added a prompt for task deletion.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 let input = document.querySelector(".input");
 let btn1 = document.querySelector(".ajouter");
 let taskadded = document.querySelector(".task-added-prompt");
+let taskDeleted = document.querySelector(".task-deleted-prompt");
 let taskparagraphe = document.createElement('p');
+let deletedPara = document.createElement('p');
 let btn2 = document.querySelector('.supprimer');
-let promptX = document.getElementById('prompt-x');
+let promptXAdd = document.getElementById('add');
+let promptXDelete = document.getElementById('delete');
 let bgBlur = document.getElementById('blur');
 
 btn1.addEventListener("click", ajouter);
@@ -28,14 +31,20 @@ function ajouter() {
 }
 
 function supprimer() {
-    if (!taskparagraphe.value) {
-
-        alert("tache supprimer avec succès !");
-    }
+    taskDeleted.append(deletedPara);
+    taskDeleted.style.display = "block";
+    deletedPara.textContent = "Tâche supprimée avec succès !";
+    bgBlur.classList.add('blur');
 }
 
-promptX.addEventListener('click', () => {
+promptXAdd.addEventListener('click', () => {
 
     taskadded.style.display = 'none';
+    bgBlur.classList.remove('blur');
+});
+
+promptXDelete.addEventListener('click', () => {
+
+    taskDeleted.style.display = 'none';
     bgBlur.classList.remove('blur');
 });
