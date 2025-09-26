c91ae8e69f6121756f847cd8f96276494d8530a8
Luna-bf <lburattifayolle@gmail.com>
2025-09-26 15:23:38 +0200

    Updated SQL requests

--- sql/todo_list.sql
+++ sql/todo_list.sql
@@ -2,6 +2,6 @@ SELECT * FROM taches;
 
 INSERT INTO taches (nom, est_complete) VALUES("Faire le ménage", FALSE);
 
-UPDATE taches SET est_complete = TRUE WHERE nom = "Faire le ménage";
+UPDATE taches SET est_complete = TRUE WHERE id_tache = 1;
 
-DELETE FROM taches WHERE nom = "Faire le ménage";
+DELETE FROM taches WHERE id_tache = 1;
